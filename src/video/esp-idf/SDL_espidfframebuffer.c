aa881b5049f47b4f5cbdd6d035ab5265c26bbed5
Juraj Michalek <juraj.michalek@gmail.com>
2024-09-09 11:43:54 +0200

    add possibility to allocate buffer in PSRAM

--- src/video/esp-idf/SDL_espidfframebuffer.c
+++ src/video/esp-idf/SDL_espidfframebuffer.c
@@ -14,9 +14,10 @@
 
 #define ESPIDF_SURFACE "SDL.internal.window.surface"
 
-static uint16_t *rgb565_buffer = NULL;
+static uint16_t *rgb_buffer = NULL;
 
 static SemaphoreHandle_t lcd_semaphore;
+static int max_chunk_height = 80;  // Configurable chunk height
 
 #ifdef CONFIG_IDF_TARGET_ESP32P4
 static bool lcd_event_callback(esp_lcd_panel_handle_t panel_io, esp_lcd_dpi_panel_event_data_t *edata, void *user_ctx)
@@ -33,34 +34,58 @@ static void lcd_event_callback(esp_lcd_panel_io_handle_t io, esp_lcd_panel_io_ev
 }
 #endif
 
+void *allocate_rgb565_buffer(size_t width, size_t height) {
+    size_t buffer_size = width * height * sizeof(uint16_t);
+
+    // First attempt to allocate in IRAM (internal memory)
+    uint16_t *rgb565_buffer = (uint16_t *)heap_caps_malloc(buffer_size, MALLOC_CAP_32BIT | MALLOC_CAP_INTERNAL);
+    if (!rgb565_buffer) {
+        // If IRAM allocation fails, attempt to allocate in PSRAM
+        printf("Failed to allocate graphical buffer in IRAM. Trying PSRAM...\n");
+        rgb565_buffer = (uint16_t *)heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
+
+        if (!rgb565_buffer) {
+            // If PSRAM allocation also fails, return an error
+            printf("Failed to allocate memory for RGB565 buffer in both IRAM and PSRAM\n");
+            return NULL;
+        } else {
+            printf("Allocation of graphical buffer in PSRAM succeeded. Warning: redraw will be slower.\n");
+        }
+    }
+
+    // Allocation was successful
+    printf("Memory allocation successful for buffer of size %zu bytes\n", buffer_size);
+    return rgb565_buffer;
+}
+
+
 int SDL_ESPIDF_CreateWindowFramebuffer(SDL_VideoDevice *_this, SDL_Window *window, SDL_PixelFormat *format, void **pixels, int *pitch)
 {
     SDL_Surface *surface;
-    const SDL_PixelFormat surface_format = SDL_PIXELFORMAT_RGB565;
     int w, h;
 
     SDL_GetWindowSizeInPixels(window, &w, &h);
-    surface = SDL_CreateSurface(w, h, surface_format);
+    surface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGB565);
     if (!surface) {
         return -1;
     }
 
     SDL_SetSurfaceProperty(SDL_GetWindowProperties(window), ESPIDF_SURFACE, surface);
-    *format = surface_format;
+    *format = SDL_PIXELFORMAT_RGB565;
     *pixels = surface->pixels;
     *pitch = surface->pitch;
 
     // Allocate RGB565 buffer in IRAM
-    rgb565_buffer = heap_caps_malloc(w * h * sizeof(uint16_t), MALLOC_CAP_32BIT | MALLOC_CAP_INTERNAL);
-    if (!rgb565_buffer) {
+    rgb_buffer = allocate_rgb565_buffer(w, h);
+    if (!rgb_buffer) {
         SDL_DestroySurface(surface);
-        return SDL_SetError("Failed to allocate memory for RGB565 buffer");
+        return SDL_SetError("Failed to allocate memory for ESP-IDF frame buffer");
     }
 
     // Create a semaphore to synchronize LCD transactions
     lcd_semaphore = xSemaphoreCreateBinary();
     if (!lcd_semaphore) {
-        heap_caps_free(rgb565_buffer);
+        heap_caps_free(rgb_buffer);
         SDL_DestroySurface(surface);
         return SDL_SetError("Failed to create semaphore");
     }
@@ -78,7 +103,6 @@ int SDL_ESPIDF_CreateWindowFramebuffer(SDL_VideoDevice *_this, SDL_Window *windo
     return 0;
 }
 
-
 IRAM_ATTR int SDL_ESPIDF_UpdateWindowFramebuffer(SDL_VideoDevice *_this, SDL_Window *window, const SDL_Rect *rects, int numrects)
 {
     SDL_Surface *surface = (SDL_Surface *)SDL_GetPointerProperty(SDL_GetWindowProperties(window), ESPIDF_SURFACE, NULL);
@@ -86,28 +110,26 @@ IRAM_ATTR int SDL_ESPIDF_UpdateWindowFramebuffer(SDL_VideoDevice *_this, SDL_Win
         return SDL_SetError("Couldn't find ESPIDF surface for window");
     }
 
-    int chunk_height = surface->h;  // Process 40 lines at a time
-    for (int y = 0; y < surface->h; y += chunk_height) {
-        int height = (y + chunk_height > surface->h) ? (surface->h - y) : chunk_height;
+    for (int y = 0; y < surface->h; y += max_chunk_height) {
+        int height = (y + max_chunk_height > surface->h) ? (surface->h - y) : max_chunk_height;
 
         // Convert to RGB565
         for (int i = 0; i < surface->w * height; i++) {
             uint16_t rgba = ((uint16_t *)surface->pixels)[y * surface->w + i];
 
 #ifdef CONFIG_IDF_TARGET_ESP32P4
-            rgb565_buffer[i] = rgba;
+            rgb_buffer[i] = rgba;
 #else
             uint8_t g = (rgba >> 11) & 0xFF;
             uint8_t r = (rgba >> 5) & 0xFF;
             uint8_t b = (rgba >> 0) & 0xFF;
-
-            rgb565_buffer[i] = ((b & 0xF8) << 8) | ((g & 0xFC) << 3) | (r >> 3);
+            rgb_buffer[i] = ((b & 0xF8) << 8) | ((g & 0xFC) << 3) | (r >> 3);
 #endif
         }
 
         // Send the chunk and wait for completion
-        ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, y, surface->w, y + height, rgb565_buffer));
-        xSemaphoreTake(lcd_semaphore, portMAX_DELAY); // Wait for the current chunk to be fully sent
+        ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, y, surface->w, y + height, rgb_buffer));
+        xSemaphoreTake(lcd_semaphore, portMAX_DELAY);  // Wait for the current chunk to be fully sent
     }
 
     return 0;
@@ -118,9 +140,9 @@ void SDL_ESPIDF_DestroyWindowFramebuffer(SDL_VideoDevice *_this, SDL_Window *win
     SDL_ClearProperty(SDL_GetWindowProperties(window), ESPIDF_SURFACE);
 
     // Free the RGB565 buffer
-    if (rgb565_buffer) {
-        heap_caps_free(rgb565_buffer);
-        rgb565_buffer = NULL;
+    if (rgb_buffer) {
+        heap_caps_free(rgb_buffer);
+        rgb_buffer = NULL;
     }
 
     // Delete the semaphore
